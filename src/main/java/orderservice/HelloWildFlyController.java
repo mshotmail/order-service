effd4a86a2836b60b7b35555431da18a70c496fa
mshotmail <msiraj@hotmail.com>
2016-09-26 14:10:45 -0400

    new

--- src/main/java/orderservice/HelloWildFlyController.java
+++ src/main/java/orderservice/HelloWildFlyController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWildFlyController {
 
 
-    @RequestMapping("hello")
+    @RequestMapping("order")
     public String sayHello(){
         return ("Hello, from orderservice");
     }
